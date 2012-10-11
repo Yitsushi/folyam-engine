c121bdd63c0738ce90e7f8f83296d9959f6599eb
Balazs Nadasdi <yitsushi@gmail.com>
2012-10-11 23:39:05 +0200

    post view base

--- models/post.js
+++ models/post.js
@@ -1,19 +1,21 @@
+var dateFormat = require('dateformat');
+
 module.exports = function(mongoose) {
   var modelObject = {};
-  
+
   var Schema   = mongoose.Schema,
       ObjectId = Schema.ObjectId;
-  
+
   var TagSchema = new Schema({
     content     : String,
     slug        : { type: String, lowercase: true, trim: true }
   });
-  
+
   TagSchema.pre('save', function(next) {
     // Todo: slug
     next();
   });
-  
+
   var PostSchema = new Schema({
     title       : String,
     slug        : { type: String, lowercase: true, trim: true },
@@ -26,7 +28,7 @@ module.exports = function(mongoose) {
     tags        : [{type: ObjectId, ref: "Tag"}],
     main_image  : { type: String, default: "" }
   });
-  
+
   PostSchema.virtual('lead_text').get(function() {
     if (this.content.match(/<br class=["']break["']\/>/)) {
       return this.content.split(/<br class=["']break["']\/>/);
@@ -35,7 +37,7 @@ module.exports = function(mongoose) {
       return this.content;
     }
   });
-  
+
   PostSchema.static('lastPostsBySite', function(site_id, limit, cb) {
     if (typeof site_id == "undefined") {
       // Todo: Implement global list
@@ -44,16 +46,40 @@ module.exports = function(mongoose) {
     if (typeof limit == "undefined" || limit === null) { limit = 7; }
     return this.find({site: site_id}).sort({created_at: -1}).limit(limit).exec(cb);
   });
-  
+
+  PostSchema.static('findPost', function(site_id, post_id, cb) {
+    if (typeof site_id == "undefined") {
+      // Todo: Implement global list
+      return cb("Site not found", null);
+    }
+    if (typeof site_id == "undefined") {
+      // Todo: Implement global list
+      return cb("Post not found", null);
+    }
+    return this.findOne({site: site_id, _id: post_id}).exec(cb);
+  });
+
+  PostSchema.virtual('path').get(function() {
+    return "/post/" + this.slug + "/" + this._id + ".html";
+  });
+
+  PostSchema.virtual('datetime').get(function() {
+    return dateFormat(this.created_at, "isoDateTime");
+  });
+
+  PostSchema.virtual('humanDate').get(function() {
+    return dateFormat(this.created_at, "fullDate");
+  });
+
   PostSchema.pre('save', function(next) {
     // Todo: slug
     next();
   });
-  
+
   modelObject.TagSchema   = TagSchema;
   modelObject.Tag         = mongoose.model('Tag', PostSchema);
   modelObject.PostSchema  = PostSchema;
   modelObject.Post        = mongoose.model('Post', PostSchema);
-  
+
   return modelObject;
 }
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,39 @@
 var services = require('../services');
     SiteModel = services.useModel('site').Site,
     PostModel = services.useModel('post').Post;
-    
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
     SiteModel.currentSite(req, function(err, currentSite) {
-      if (currentSite === null) { return SiteModel.render404(req, res); }
+      if (currentSite === null) {
+        return SiteModel.render404(req, res);
+      }
       PostModel.lastPostsBySite(currentSite._id, 7, function(err, posts) {
         return res.render('page/index', { title: 'Android Jelly Bean-t pakoltam Galaxy S-re', posts: posts });
       });
-    })
+    });
   });
 
+
+  app.get('/post/*/*.html', function(req, res) {
+    SiteModel.currentSite(req, function(err, currentSite) {
+      if (currentSite === null) {
+        return SiteModel.render404(req, res);
+      }
+
+      PostModel.findPost(currentSite._id, req.params[1], function(err, post) {
+        if (post) {
+          return res.render('page/post', { title: post.title, post: post });
+        }
+        return SiteModel.render404(req, res);
+      });
+    });
+  });
+/*
   app.get('/create_posts', function(req, res) {
     SiteModel.currentSite(req, function(err, currentSite) {
       if (currentSite === null) { return SiteModel.render404(req, res); }
-      
+
       var Author = services.useModel('author').Author;
       Author.findOne({username: "yitsushi"}, function(err, me) {
         p = new PostModel();
@@ -24,7 +42,7 @@ module.exports = function(app) {
         p.author      = me._id;
         p.site        = currentSite._id;
         p.main_image  = 'http://folyam.info/assets/img/2012-08-13-android-jelly-bean-et-pakoltam-a-samsung-galaxy-s-emre-i9000/header.jpeg';
-        
+
         p.save(function(err) {
           if (err) {
             console.log(err);
@@ -35,7 +53,7 @@ module.exports = function(app) {
       });
     });
   });
-/*
+
   app.get('/create_sites', function(req, res) {
     var Site = services.useModel('site').Site;
     var Author = services.useModel('author').Author;
@@ -62,28 +80,28 @@ module.exports = function(app) {
         if (err) {
           console.log(err);
         }
-        
+
         me.sites.addToSet(s1._id);
 
         s2.save(function(err) {
           if (err) {
             console.log(err);
           }
-          
+
           me.sites.addToSet(s2._id);
 
           s3.save(function(err) {
             if (err) {
               console.log(err);
             }
-            
+
             me.sites.addToSet(s3._id);
-            
+
             me.save(function(err) {
               if (err) {
                 console.log(err);
               }
-              
+
               res.redirect('/');
             });
           });
@@ -91,7 +109,7 @@ module.exports = function(app) {
       });
     });
   });
-  
+
   app.get('/create_me', function (req, res) {
     var model = services.useModel('author');
     var me = new model.Author();
@@ -100,12 +118,12 @@ module.exports = function(app) {
     me.location = 'Budapest, HU';
     me.email = 'yitsushi@folyam.info';
     me.googlePlusId = '104695723888883478740';
-    
+
     me.save(function(err) {
       if (err) {
         console.log(err);
       }
-      
+
       res.redirect('/');
     });
   });
